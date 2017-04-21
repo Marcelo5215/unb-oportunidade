be2c8d90c55320078978fecae942d11fec1d18a9
Mateus Luiz <mateuslfreitasb@gmail>
2017-04-21 19:40:12 -0300

    Mudando cara as urls e tentando arrumando erro 405 ao tentar gerar token

--- unb_oportunidade_src/urls.py
+++ unb_oportunidade_src/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^(?P<path>.*)/$', IndexView.as_view()),
     url(r'^api/exemplo$', ExempleView.as_view(), name='get'),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api/api-token-auth/', obtain_jwt_token),
+    url(r'^api/api-token-verify/', verify_jwt_token),
+    url(r'^api/api-token-refresh/', refresh_jwt_token),
 
 ]
