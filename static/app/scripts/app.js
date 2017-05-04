bdf5310e8d85dc3b265ac0b45fe1470dc9a7b6cc
Mateus Luiz <mateuslfb@hotmail.com>
2017-05-04 17:31:40 -0300

    Setando página inicial

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -7,6 +7,15 @@ angular.module('unbOportunidade', [
 function appConfig($httpProvider, $urlRouterProvider, $stateProvider) {
 
   $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('home', {
+      url: '/',
+      views: {
+        '@content': {
+            templateUrl: 'static/app/scripts/index.html'
+        }
+      }
+    })
 
   $httpProvider.defaults.cache = true;
 }
