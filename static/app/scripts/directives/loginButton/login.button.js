e78a8aaba6020581339defea761eb7eee1a9fdd8
Mateus Luiz <mateuslfreitasb@gmail>
2017-05-06 15:41:08 -0300

    Modal para tela de login sendo feito

---
+++ static/app/scripts/directives/loginButton/login.button.js
@@ -0,0 +1,27 @@
+angular.module('unbOportunidade')
+  .directive('loginButton', function($uibModal) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+
+        scope.$on('toggleLoginModal', function () {
+          modalInitialization();
+        });
+
+        element.bind('click', function () {
+          console.log('entrei');
+          scope.$broadcast('toggleLoginModal');
+        });
+
+        function modalInitialization(){
+          $uibModal.open({
+            templateUrl: 'static/app/scripts/directives/loginButton/login-modal.html',
+            controller: function ($scope) {
+
+            }
+          });
+        }
+
+      }
+    }
+  });
