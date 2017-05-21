34616f97721d874f1ce38db2b5ef7257271249d8
PedroAcA <pedro.almeidabsb@gmail.com>
2017-05-21 00:37:24 -0300

    Iniciando o processo de busca inicial (comecando a sair da fase de esboco)

--- api/Busca/Initial_search.py
+++ api/Busca/Initial_search.py
@@ -1,18 +1,22 @@
 import sqlite3
-def tokenize_db_search(string):
-	str_tok = str(string).split("'")
-	return str_tok[1]
-	
-conn = sqlite3.connect("teste.db")
-c = conn.cursor()#cursor para pesquisa na tablea de alunos
-c2 = conn.cursor()#cursor para pesquisa na tabela de empresas
-student_name = 'Joao'
-courses = c.execute("SELECT Curso FROM Alunos WHERE Nome = ?",[student_name])
-for course in courses:
-	course_tok  = tokenize_db_search(course)
-	companies = c2.execute("SELECT Empresa FROM Empresas_Cursos WHERE Curso = ?",[course_tok])
+#Versao desse programa realiza a busca do curso do aluno e imprime na tela o nome e a abreviacao do curso. Atualizar apara pegar lista de oferta das empresas	
+conn = sqlite3.connect("../../db.sqlite3")
+c = conn.cursor()
+c2 = conn.cursor()
+c3 = conn.cursor()
+surname = 'Almeida'
+name = 'Pedro'
+student_info = c.execute("SELECT cpf FROM students WHERE first_name=? AND last_name = ?",(name,surname))
+for info in student_info:
+	#print(info[0])
+	curriculum_info = c2.execute("SELECT course_id_id FROM Curriculum WHERE cpf_id = ?",[info[0]])
 
-for company in companies:
-	print (tokenize_db_search(company))
+for course_id in curriculum_info:
+	#print(course_id[0])
+	course_info = c3.execute("SELECT name,abbreviation FROM Courses where id_course=?",[course_id[0]])
+
+for course_name in course_info:
+	print(course_name[0],',',course_name[1])#pega o curso do aluno com sobrenome indicado na variavel surname e imprime na tela a abreviacao do nome do curso e nome completo do curso 
+
 conn.close()
 
