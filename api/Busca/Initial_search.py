2c5114bdd7f68a3df687cc51c66f5586ff1647ce
PedroAcA <pedro.almeidabsb@gmail.com>
2017-05-21 18:07:46 -0300

    Primeira tarefa do cronograma definido no Trello Dev|Busca

--- api/Busca/Initial_search.py
+++ api/Busca/Initial_search.py
@@ -1,22 +1,35 @@
 import sqlite3
-#Versao desse programa realiza a busca do curso do aluno e imprime na tela o nome e a abreviacao do curso. Atualizar apara pegar lista de oferta das empresas	
+#Versao desse programa realiza a busca da lista de oferta das empresas cujo curso pre-requisito eh compativel com o curso do aluno. Atualizar para a primeira busca ja ser com o user ID	
 conn = sqlite3.connect("../../db.sqlite3")
 c = conn.cursor()
 c2 = conn.cursor()
 c3 = conn.cursor()
+c4 = conn.cursor()
+c5 = conn.cursor()
 surname = 'Almeida'
 name = 'Pedro'
 student_info = c.execute("SELECT cpf FROM students WHERE first_name=? AND last_name = ?",(name,surname))
-for info in student_info:
-	#print(info[0])
+for info in student_info:	
 	curriculum_info = c2.execute("SELECT course_id_id FROM Curriculum WHERE cpf_id = ?",[info[0]])
 
 for course_id in curriculum_info:
-	#print(course_id[0])
-	course_info = c3.execute("SELECT name,abbreviation FROM Courses where id_course=?",[course_id[0]])
+	vacant_job_info = c3.execute("SELECT vacant_job_id_id FROM Vacant_job_has_course where course_id_id=?",[course_id[0]])
 
-for course_name in course_info:
-	print(course_name[0],',',course_name[1])#pega o curso do aluno com sobrenome indicado na variavel surname e imprime na tela a abreviacao do nome do curso e nome completo do curso 
+companies_cnpj=[]
+
+for vacant_job in vacant_job_info:
+	sql_companies_id = c4.execute("SELECT id_company_id FROM Hiring where id_vacancy_id=?",[vacant_job[0]])
+	for sql_company_id in sql_companies_id:
+		companies_cnpj.append(sql_company_id[0])
+	
+companies_name = []
+for company_id in companies_cnpj:
+	sql_companies_name = c5.execute("SELECT name FROM Companies where cnpj=?",[company_id])
+	for sql_company_name in sql_companies_name:	
+		companies_name.append(sql_company_name[0])
+
+for company in companies_name:
+	print(company)#pega o nome das empresas cujo curso pre-requisito eh compativel com o curso do aluno 
 
 conn.close()
 
