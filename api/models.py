e5a0c72512e3a34b55a255fcdf50dc3b45b1d211
Roberta Renally da Silva Melo <robertarenallydsm@gmail.com>
2017-05-25 12:32:16 -0300

    Adicionando user_id na tabela Student

--- api/models.py
+++ api/models.py
@@ -7,23 +7,6 @@ from django.core.validators import RegexValidator
 
 
 
-
-
-class Address(models.Model):
-	adress_id = models.AutoField(primary_key=True)
-	city = models.CharField(max_length=45)
-	neighborhood = models.CharField(max_length=45)
-	number = models.CharField(max_length=4)
-	complement = models.CharField(max_length=45)
-	zip_code = models.CharField(max_length=8)
-	public_place = models.CharField(max_length=45)
-	class Meta:
-		db_table = 'Address'
-
-	def __unicode__(self):
-		return '{}'.format(self.address_id)
-
-
 class Student(models.Model):
 	cpf = models.CharField(primary_key=True, max_length=11, validators=[RegexValidator(r'^\d{11}$')])
 	first_name = models.CharField(max_length=45)
@@ -38,6 +21,20 @@ class Student(models.Model):
 	def __unicode__(self):
 		return '[{}] {}'.format(self.id_user,self.cpf)
 
+class Address(models.Model):
+	id_adress = models.AutoField(primary_key=True)
+	city = models.CharField(max_length=45)
+	neighborhood = models.CharField(max_length=45)
+	number = models.CharField(max_length=4)
+	complement = models.CharField(max_length=45)
+	zip_code = models.CharField(max_length=8)
+	public_place = models.CharField(max_length=45)
+	class Meta:
+		db_table = 'Address'
+
+	def __unicode__(self):
+		return '{}'.format(self.id_adress)
+
 
 class Company(models.Model):
 	cnpj = models.AutoField(primary_key=True)
@@ -46,42 +43,14 @@ class Company(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
 	address_id = models.ForeignKey(Address)
-	id_user = models.OneToOneField(User)
+	id_user = models.ForeignKey(User,unique=True)
+	phone_number = models.CharField(max_length=45)
 	agreement = models.BooleanField()
 	class Meta:
 		db_table = 'Companies'
 
 	def __unicode__(self):
 		return '{} [{}]'.format(self.cnpj,self.address_id)
-
-
-class Phone(models.Model):
-    id_phone = models.AutoField(primary_key=True)
-    phone_number = models.CharField(max_length=15)
-    class Meta:
-        db_table = 'Phone'
-    def __unicode__(self):
-        return '{}'.format(self.id_phone)
-
-
-class Student_has_phone(models.Model):
-    student_id = models.ForeignKey(Student)
-    phone_id = models.ForeignKey(Phone)
-    class Meta:
-        unique_together = ('student_id','phone_id')
-
-    def __unicode__(self):
-        return '[{}] [{}]'.format(self.student_id, self.phone_id)
-
-
-class Company_has_phone(models.Model):
-    company_id = models.ForeignKey(Company)
-    phone_id = models.ForeignKey(Phone)
-    class Meta:
-        unique_together = ('company_id','phone_id')
-
-    def __unicode__(self):
-        return '[{}] [{}]'.format(self.company_id, self.phone_id)
 
 
 class File(models.Model):
@@ -101,7 +70,7 @@ class Course(models.Model):
 
 
 class Curriculum(models.Model):
-	cpf = models.OneToOneField(Student,primary_key=True)
+	cpf = models.ForeignKey(Student,primary_key=True)
 	time_desired = models.IntegerField()
 	work_shift = models.CharField(max_length=20)
 	file_id = models.ForeignKey(File)
@@ -117,19 +86,6 @@ class Curriculum(models.Model):
 		return '[{}] [{}] {}'.format(self.cpf,self.course_id,self.file_id)
 
 
-class Requirements(models.Model):
-	id_requirements = models.AutoField(primary_key=True)
-	work_shift = models.CharField(max_length=20)
-	workload = models.IntegerField()
-	additional_info = models.CharField(max_length=500)
-	minimun_period = models.IntegerField()
-	class Meta:
-		db_table = 'Requirements'
-
-	def __unicode__(self):
-		return '{}'.format(self.id_requirements)
-
-
 class Vacant_job(models.Model):
 	id_vacancy = models.AutoField(primary_key=True)
 	role = models.CharField(max_length=200)
@@ -141,39 +97,42 @@ class Vacant_job(models.Model):
 	def __unicode__(self):
 		return '{}'.format(self.id_vacancy)
 
-
 class Vacant_job_has_course(models.Model):
-	vacant_job_id = models.ForeignKey(Vacant_job)
+	vacant_job_id = models.ForeignKey(Student,primary_key=True)
 	course_id = models.ForeignKey(Course)
 	class Meta:
-		unique_together = ('vacant_job_id','course_id')
+		db_table = 'Vacant_job_has_course'
 
 	def __unicode__(self):
 		return '[{}] [{}]'.format(self.vacant_job_id,self.course_id)
-
-
-class Vacant_job_has_requirement(models.Model):
-    vacant_job_id = models.ForeignKey(Vacant_job)
-    requirement_id = models.ForeignKey(Requirements)
-    class Meta:
-        unique_together = ('vacant_job_id','requirement_id')
-    def __unicode__(self):
-        return '[{}] [{}]'.format(self.vacant_job_id,self.requirement_id)
-
+	
 
 class Hiring(models.Model):
 	id_hiring = models.AutoField(primary_key=True,)
-	contracted_at = models.DateTimeField()
-	contract_end = models.DateTimeField()
+	contracted_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+	contract_end = models.DateTimeField(auto_now_add=True, auto_now=False)
 	active = models.BooleanField()
 	id_student = models.ForeignKey(Student)
 	id_company = models.ForeignKey(Company)
-	id_vacant_job = models.ForeignKey(Vacant_job)
+	id_vacancy = models.ForeignKey(Vacant_job)
 	class Meta:
 		db_table = 'Hiring'
 
 	def __unicode__(self):
-		return '[{}] [{}] [{}] {}'.format(self.id_student,self.id_company,self.id_vacanct_job,self.id_hiring)
+		return '[{}] [{}] [{}] {}'.format(self.id_student,self.id_company,self.id_vacancy,self.id_hiring)
+
+
+class Requirements(models.Model):
+	id_requirements = models.AutoField(primary_key=True)
+	work_shift = models.CharField(max_length=20)
+	workload = models.IntegerField()
+	additional_info = models.CharField(max_length=500)
+	minimun_period = models.IntegerField()
+	class Meta:
+		db_table = 'Requirements'
+
+	def __unicode__(self):
+		return '{}'.format(self.id_requirements)
 
 
 class Review(models.Model):
@@ -210,4 +169,5 @@ class Bank_accounts(models.Model):
 		db_table = 'Bank_accounts'
 
 	def __unicode__(self):
-return '{} [{}] [{}]'.format(self.id_bank_accounts,self.bank_names_bank_number,self.student_cpf)
+		return '{} [{}] [{}]'.format(self.id_bank_accounts,self.bank_names_bank_number,self.student_cpf)
+
