56c3118b84fdfc349b1ca75a231c4dfcbf179cf5
vamcs <victoraugusto16@gmail.com>
2017-06-17 17:56:03 -0300

    Adiciona service para vagas.

--- static/app/scripts/services/vacancies.service.js
+++ static/app/scripts/services/vacancies.service.js
@@ -2,20 +2,35 @@ angular.module('unbOportunidade')
   .service('vacanciesService', ['$http', VacanciesService]);
 
 function VacanciesService($http) {
-  this.getAllVacancies = function() {
-    return $http.get('api/vagas/');
+
+  // Retorna todas as vagas ativas do sistema.
+  this.getAllActiveVacancies = function() {
+    return $http.get('api/vagas/?is_ativa=true');
   };
 
+  // Retorna vaga específica pelo id.
   this.getVacancy = function(id) {
     return $http.get('api/vagas/' + id);
   };
 
-  this.getEnterpriseVacancies = function(enterprise_id) {
-    return $http.get('api/vagas/?empresa=' + enterprise_id);
+  // Retorna todas as vagas de uma empresa (ativas e não ativas)
+  this.getAllEnterpriseVacancies = function (enterprise_id) {
+    return $http.get('api/vagas/?empresa=' + enterprise_id)
   };
 
-  this.filterCourseVacancies = function (course_id) {
-    return $http.get('api/vagas/?curso=' + course_id);
-  }
+  // Retorna todas as vagas ativas de uma empresa.
+  this.getActiveEnterpriseVacancies = function(enterprise_id) {
+    return $http.get('api/vagas/?is_ativa=true&empresa=' + enterprise_id);
+  };
+
+  // Retorna todas as vagas ativas de acordo com um curso.
+  this.getActiveCourseVacancies = function(course_id) {
+    return $http.get('api/vagas/?is_ativa=true&curso=' + course_id);
+  };
+
+  // Retorna todas as vagas ativas de acordo com curso e empresa.
+  this.getActiveCourseEnterpriseVacancies = function(course_id, enterprise_id) {
+    return $http.get('api/vagas/?is_ativa=true&curso=' + course_id + '&empresa=' + enterprise_id);
+  };
 
 }
